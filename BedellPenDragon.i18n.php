ff433e0f160b8eff548662f987f9b9f38870070d
Karsten Hoffmeyer <mediawiki@kghoffmeyer.de>
2014-01-31 15:07:13 +0100

    Update BedellPenDragon.i18n.php
    * Add extension's description
    * Add German translation

--- BedellPenDragon.i18n.php
+++ BedellPenDragon.i18n.php
@@ -5,6 +5,9 @@
 
 $magicWords = array();
 
+/** English
+* @author Nathan Larson (Leucosticte)
+*/
 $magicWords['en'] = array(
    'setbpdprop' => array( 0, 'setbpdprop' ),
    'getbpdprop' => array( 0, 'getbpdprop' )
@@ -12,17 +15,40 @@ $magicWords['en'] = array(
 
 $messages = array();
 
+/** English
+* @author Nathan Larson (Leucosticte)
+*/
 $messages['en'] = array(
-    'glossary' => 'Glossary',
-    'essaylist' => 'Essay list',
-    'miscellanylist' => 'Miscellany list',
-    'quotelist' => 'Quote list',
-    'videolist' => 'Video list',
-    'bedellpendragon-noitems' => 'There are no items to display.',
-    'glossary-url' => 'Special:Glossary',
-    'essaylist-url' => 'Special:EssayList',
-    'quotelist-url' => 'Special:QuoteList',
-    'miscellanylist-url' => 'Special:MiscellanyList',
-    'videolist-url' => 'Special:VideoList',
-    'bedellpendragon-byline' => ' by '
+   'bedellpendragon-desc' => 'Adds the "<code>randompageincat</code>" tag as well as the "<code>#setbpdprop</code>" and "<code>#getbpdprop</code>" parser functions and special pages',
+   'glossary' => 'Glossary',
+   'essaylist' => 'Essay list',
+   'miscellanylist' => 'Miscellany list',
+   'quotelist' => 'Quote list',
+   'videolist' => 'Video list',
+   'bedellpendragon-noitems' => 'There are no items to display.',
+   'glossary-url' => 'Special:Glossary',
+   'essaylist-url' => 'Special:EssayList',
+   'quotelist-url' => 'Special:QuoteList',
+   'miscellanylist-url' => 'Special:MiscellanyList',
+   'videolist-url' => 'Special:VideoList',
+   'bedellpendragon-byline' => ' by '
+);
+
+/** German (Deutsch)
+* @author Kghbln
+*/
+$messages['de'] = array(
+   'bedellpendragon-desc' => 'Ergänzt das Tag „<code>randompageincat</code>“, die Parserfunktionen „<code>#setbpdprop</code>“ und „<code>#getbpdprop</code>“ sowie diverse Spezialseiten',
+   'glossary' => 'Glossar',
+   'essaylist' => 'Liste der Essays',
+   'miscellanylist' => 'Liste sonstiger Seiten',
+   'quotelist' => 'Liste der Zitate',
+   'videolist' => 'Liste der Videos',
+   'bedellpendragon-noitems' => 'Es ist nichts zum Anzeigen vorhanden.',
+   'glossary-url' => 'Spezial:Glossar',
+   'essaylist-url' => 'Spezial:Liste_der_Essays',
+   'quotelist-url' => 'Spezial:Liste_der_Zitate',
+   'miscellanylist-url' => 'Spezial:Liste_sonstiger_Seiten',
+   'videolist-url' => 'Spezial:Liste_der_Videos',
+   'bedellpendragon-byline' => ' von '
 );
